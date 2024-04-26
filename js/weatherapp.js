94acc2e379d02a2ce53733217ad796659a9e9cb1
mika.stenberg@laurea.fi <mika.stenberg@laurea.fi>
2024-04-26 12:51:52 +0300

    add

--- js/weatherapp.js
+++ js/weatherapp.js
@@ -1,28 +1,27 @@
-let url = "https://api.openweathermap.org/data/2.5/weather?q=Helsinki&units=metric&mode=json&APPID=ff64c247a136f706923d1ee0d55d71e2"
+let url =
+  "https://api.openweathermap.org/data/2.5/weather?q=Helsinki&units=metric&mode=json&APPID=ff64c247a136f706923d1ee0d55d71e2";
 
 // Luodaan ja lähetetään AJAX kutsu
 var xmlhttp = new XMLHttpRequest();
-xmlhttp.open("GET",url,true);
+xmlhttp.open("GET", url, true);
 xmlhttp.send();
 // Jäädään odottamaan vastausta
-xmlhttp.onreadystatechange=function() {
-    if (xmlhttp.readyState==4 && xmlhttp.status==200){
-        // Tulostetaan se ruudulle
-       // document.write( xmlhttp.responseText)
-        // Kutsutaan funktiota joka hoitaa loput
-        parsiData( xmlhttp.responseText );
+xmlhttp.onreadystatechange = function () {
+  if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+    // Tulostetaan se ruudulle
+    // document.write( xmlhttp.responseText)
+    // Kutsutaan funktiota joka hoitaa loput
+    parsiData(xmlhttp.responseText);
   }
-}
+};
 
-function parsiData(data){
+function parsiData(data) {
+  // Muutetaan teksti JSONiksi
+  json = JSON.parse(data);
+  // console.log( json.name, json.main.temp, json.weather[0].main )
 
-    // Muutetaan teksti JSONiksi
-    json = JSON.parse( data )
-    // console.log( json.name, json.main.temp, json.weather[0].main )
-
-    // Esitellään ulkoasu muuttujan sisälle
-   let html = 
-   `
+  // Esitellään ulkoasu muuttujan sisälle
+  let html = `
    <h1>Sääsovellus v. 1.1 </h1>
    <table border="1" width="100%">
     <tr>
@@ -44,8 +43,8 @@ function parsiData(data){
         <td><img src="http://openweathermap.org/img/w/${json.weather[0].icon}.png"></td>
     </tr>
    </table>
-   `
-   // Tulostetaan ruudulle
-   document.write(html)
-
+   `;
+  // Etsitään elementti johon sivu sijoitetaan
+  //document.write(html);
+  document.querySelector('#main').innerHTML = html;
 }
