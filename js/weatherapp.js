47341ffc0c207658641838e5f3e899d76427434f
mika.stenberg@laurea.fi <mika.stenberg@laurea.fi>
2024-04-26 13:14:22 +0300

    add

--- js/weatherapp.js
+++ js/weatherapp.js
@@ -1,20 +1,30 @@
-let url =
-  "https://api.openweathermap.org/data/2.5/weather?q=Helsinki&units=metric&mode=json&APPID=ff64c247a136f706923d1ee0d55d71e2";
+// Lataa Helsingin sää alkusivulle
+getWeather("Helsinki");
 
-// Luodaan ja lähetetään AJAX kutsu
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
-// Jäädään odottamaan vastausta
-xmlhttp.onreadystatechange = function () {
-  if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-    // Tulostetaan se ruudulle
-    // document.write( xmlhttp.responseText)
-    // Kutsutaan funktiota joka hoitaa loput
-    parsiData(xmlhttp.responseText);
-  }
-};
+// Lue tekstikentän tiedot
+function getCity() {
+  let city = document.querySelector("#city").value;
+  getWeather(city);
+}
 
+// Tee AJAX kutsu
+function getWeather(city) {
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&mode=json&APPID=ff64c247a136f706923d1ee0d55d71e2`;
+
+  // Luodaan ja lähetetään AJAX kutsu
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+  // Jäädään odottamaan vastausta
+  xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      // Tulostetaan se ruudulle
+      // document.write( xmlhttp.responseText)
+      // Kutsutaan funktiota joka hoitaa loput
+      parsiData(xmlhttp.responseText);
+    }
+  };
+}
 function parsiData(data) {
   // Muutetaan teksti JSONiksi
   json = JSON.parse(data);
@@ -46,5 +56,5 @@ function parsiData(data) {
    `;
   // Etsitään elementti johon sivu sijoitetaan
   //document.write(html);
-  document.querySelector('#main').innerHTML = html;
+  document.querySelector("#main").innerHTML = html;
 }
